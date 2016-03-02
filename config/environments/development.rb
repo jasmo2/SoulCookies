aad5bdf0d6984577f6a3fea46148f30fe3bb2f95
jasmo2 <jasmo2.com@gmail.com>
2016-03-02 13:20:11 -0500

    configure mailer in proyect

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,16 +15,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => "gmail.com",
-      :user_name            => ENV["SOULCOOKIES_EMAIL_U"],
-      :password             => ENV["SOULCOOKIES_EMAIL_P"],
-      :authentication       => :plain,
-      :enable_starttls_auto => true
-  }
+  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.smtp_settings = {
+  #     :address              => "smtp.gmail.com",
+  #     :port                 => 587,
+  #     :domain               => "gmail.com",
+  #     :user_name            => ENV["SOULCOOKIES_EMAIL_U"],
+  #     :password             => ENV["SOULCOOKIES_EMAIL_P"],
+  #     :authentication       => :plain,
+  #     :enable_starttls_auto => true
+  # }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
