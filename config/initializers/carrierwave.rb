631bf842eeadf9e0270924f4b5af0d900156200c
jasmo2 <jasmo2.com@gmail.com>
2016-03-03 16:59:17 -0500

    refactor carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,6 +7,7 @@ CarrierWave.configure do |cfg|
         :aws_access_key_id      => ENV["AWS_ACCESS_ID"],
         :aws_secret_access_key  => ENV["AWS_ACCESS_SECRET"]
     }
+    cfg.fog_directory  = ENV["AWS_BUCKET"]
     cfg.fog_public     = true
     cfg.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   else
