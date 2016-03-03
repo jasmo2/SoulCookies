8d9ecbbaaca5ec5195bce50e4871e59e01515ba6
jasmo2 <jasmo2.com@gmail.com>
2016-03-03 16:42:51 -0500

    refactor carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,10 @@ CarrierWave.configure do |cfg|
     cfg.storage = :fog
     cfg.fog_credentials = {
         :provider               => 'AWS',
-        :region                 => "eu-west-1",
+        :region                 => "us-west-2",
         :aws_access_key_id      => ENV["AWS_ACCESS_ID"],
         :aws_secret_access_key  => ENV["AWS_ACCESS_SECRET"]
     }
-    cfg.fog_directory  = ENV["AWS_BUCKET"]
     cfg.fog_public     = true
     cfg.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   else
