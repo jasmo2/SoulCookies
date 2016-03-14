f393799032270600b087e2862b38a5e3a35b809d
jasmo2 <jasmo2.com@gmail.com>
2016-03-14 17:33:19 -0500

    add helper method "i18n_attributes"

--- app/controllers/API/base_controller.rb
+++ app/controllers/API/base_controller.rb
@@ -1,5 +1,13 @@
 module  Api
   class BaseController < ApplicationController
     protect_from_forgery with: :null_session
+    def i18n_attributes(klass, instance)
+      i18l_hash = {}
+      instance.errors.messages.each do |k,v|
+        i18l_hash["#{klass.human_attribute_name(k)}"] = v.first
+      end
+      i18l_hash
+    end
+    helper_method :i18n_attributes
   end
 end
