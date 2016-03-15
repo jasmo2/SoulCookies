638b41da28afeefe28bd6af318d9863b833add03
jasmo2 <jasmo2.com@gmail.com>
2016-03-14 22:04:59 -0500

    cookietracker perform_at

--- app/jobs/cookie_tracker_job.rb
+++ app/jobs/cookie_tracker_job.rb
@@ -6,8 +6,11 @@ class CookieTrackerJob < ActiveJob::Base
     if state.seq < 4
       time  = ENV['STATE_TIMER'] || 3
       time = time.to_i
+      puts " Time_now: #{Time.now.to_f}"
+      puts "state: #{state}"
       # CookieTrackerJob.perform_in(time.minutes, state)
-      CookieTrackerJob.set(wait: time.minutes).perform_later(state.increment_seq)
+      CookieTrackerJob.perform_at(time.minutes.from_now, state)
+      # CookieTrackerJob.perform_at(state.increment_seq)
     end
   end
 end
