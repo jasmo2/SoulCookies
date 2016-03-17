4f11ba645deb5d3142a896d03a52bb00fa624b42
jasmo2 <jasmo2.com@gmail.com>
2016-03-17 11:59:20 -0500

    add skip_before_filter  :verify_authenticity_token

--- app/controllers/API/base_controller.rb
+++ app/controllers/API/base_controller.rb
@@ -1,5 +1,7 @@
 module  Api
   class BaseController < ApplicationController
+
+    skip_before_filter  :verify_authenticity_token
     protect_from_forgery with: :null_session
     def i18n_attributes(klass, instance)
       i18l_hash = {}
