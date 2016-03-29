bf6abd597841d9ece42b228fa8c65a54127fc552
jasmo2 <jasmo2.com@gmail.com>
2016-03-29 11:02:17 -0500

    Cookietracker job

--- app/models/api/order_api.rb
+++ app/models/api/order_api.rb
@@ -21,9 +21,14 @@ module Api
                                     })
       begin
         @order.confirm!
-        OrderMailer.delay.received_order(@order)
-        OrderMailer.delay.new_order_admin(Shoppe::User.all,@order.id)
-        state = State.create(order_tracker_id: @order.id)
+        # OrderMailer.delay.received_order(@order)
+        # OrderMailer.delay.new_order_admin(Shoppe::User.all,@order.id)
+        state =  State.new(order_tracker_id: @order.id)
+        if state.save
+          CookieTrackerJob.perform_now(state)
+        else
+          raise Exception, "State not save"
+        end
         @order.number
       rescue Exception => e
         raise Exception, e
