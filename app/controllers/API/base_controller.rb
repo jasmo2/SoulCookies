cae6cfa34c384eeb5f4aca0bf4d6ecd3c60a9ff5
jasmo2 <jasmo2.com@gmail.com>
2016-04-06 11:21:02 -0500

    Add token handle

--- app/controllers/API/base_controller.rb
+++ app/controllers/API/base_controller.rb
@@ -1,8 +1,28 @@
 module  Api
   class BaseController < ApplicationController
-
+    attr_accessor :current_customer
     skip_before_filter  :verify_authenticity_token
     protect_from_forgery with: :null_session
 
+    def current_customer
+      @current_customer
+    end
+
+    protected
+
+    def access_private
+      authenticate_token || render_unauthorized
+    end
+
+    def authenticate_token
+      authenticate_with_http_token do |token, options|
+        @current_customer = CustomersUser.find_by_oauth_token(token)
+      end
+    end
+
+    def render_unauthorized
+      self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+      render json: {"error": "credenciales invalidas"}, status: :unauthorized
+    end
   end
 end
