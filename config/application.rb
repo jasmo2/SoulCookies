2d7d48928af1b33a13c3208b82c2d34fd2d70187
jasmo2 <jasmo2.com@gmail.com>
2016-04-12 14:19:31 -0500

    update CORS dependency

--- config/application.rb
+++ config/application.rb
@@ -38,11 +38,16 @@ module Shoppecookies
         :authentication       => :plain,
         :enable_starttls_auto => true
     }
-
-    config.action_dispatch.default_headers = {
-        'Access-Control-Allow-Origin' => '*',
-        'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
-        'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
-    }
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
+    # config.action_dispatch.default_headers = {
+    #     'Access-Control-Allow-Origin' => '*',
+    #     'Access-Control-Request-Method' => 'GET, PATCH, PUT, POST, OPTIONS, DELETE',
+    #     'Access-Control-Allow-Headers:' => 'Origin, X-Requested-With, Content-Type, Accept'
+    # }
   end
 end
