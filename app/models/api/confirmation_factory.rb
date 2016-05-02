57bfc93ad48232d80d6106d63140f66f5b63d0c5
jasmo2 <jasmo2.com@gmail.com>
2016-05-02 10:52:31 -0500

    fix express arguments

--- app/models/api/confirmation_factory.rb
+++ app/models/api/confirmation_factory.rb
@@ -15,15 +15,13 @@ module Api
       request_ip = agrs[:request_ip]
       order = Shoppe::Order.find_by_id(current_order.id)
       order.separate_delivery_address = "0"
-      order.attributes = order_params
-      order.attributes = {
-          billing_address2: "Colombia",
-          billing_address3: "Bogotá",
-          billing_address4: "Bogotá D.C.",
-          billing_country_id: Shoppe::Country.where(name: "Colombia").first.id,
-          billing_postcode: "4-72",
-          last_name: "."
-      }
+      order.attributes = order_params.merge({
+                                                billing_address2: "Colombia",
+                                                billing_address3: "Bogotá",
+                                                billing_address4: "Bogotá D.C.",
+                                                billing_country_id: Shoppe::Country.where(name: "Colombia").first.id,
+                                                billing_postcode: "4-72"
+                                            })
       order.ip_address = request_ip
       unless order.proceed_to_confirm
         raise Api::Exceptions::InappropriateAddress ,  Api::Exceptions::I18n_m.new(Shoppe::Order, order).i18n_attributes
